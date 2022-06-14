6ddc0cafefceb89734f1f1e0f4991ce7970f5a66
chatomzcoding <coding.chatomz@gmail.com>
2022-06-14 10:04:05 +0700

    update dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,17 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Artikel;
 use App\Models\Fasilitas;
 use App\Models\Guru;
-use App\Models\Kelas;
-use App\Models\Kontrak;
-use App\Models\Mapel;
-use App\Models\Pekerjaan;
-use App\Models\Perusahaan;
 use App\Models\Program;
 use App\Models\Siswa;
-use App\Models\Slider;
-use App\Models\Tagline;
-use App\Models\Timlokus;
-use App\Models\Waktu;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -26,10 +17,10 @@ class HomeController extends Controller
     {
         $menu   = 'dashboard';
         $statistik  = [
-            'kontrak' => 1,
-            'timlokus' => 2,
-            'pekerjaan' => 3,
-            'perusahaan' => 4,
+            'kontrak' => Siswa::count(),
+            'timlokus' => Guru::count(),
+            'pekerjaan' => Fasilitas::count(),
+            'perusahaan' => Program::count(),
         ];
         return view('admin.dashboard', compact('menu','statistik'));
     }
